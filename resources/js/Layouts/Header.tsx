176b64c25c5747c12717488eaf82b6f5d7f05b87
戴日东 <550398445@qq.com>
2023-07-14 18:31:58 +0800

    edit navigation

--- resources/js/Layouts/Header.tsx
+++ resources/js/Layouts/Header.tsx
@@ -1,32 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from '@inertiajs/react';
-import axios from 'axios';
 import styles from './styles.module.css';
 import Navigation from './Navigation';
-import { Category } from '@/types/models';
 
-const Header = () => {
-  const [categories, setCategories] = useState<Category[]>([]);
-
-  useEffect(() => {
-    (async () => {
-      const response = await axios.get(route('categories.index'));
-      setCategories(response.data.data);
-    })();
-  }, []);
-
-  return (
-    <div className={styles.header}>
-      {/* Logo */}
-      <div>
-        <Link href={route('home')} className={styles.logo}>
-          DRD
-        </Link>
-      </div>
-
-      <Navigation categories={categories} />
+const Header = () => (
+  <div className={styles.header}>
+    {/* Logo */}
+    <div>
+      <Link href={route('home')} className={styles.logo}>
+        DRD
+      </Link>
     </div>
-  );
-};
+
+    <Navigation />
+  </div>
+);
 
 export default Header;
--- resources/js/Layouts/Navigation.tsx
+++ resources/js/Layouts/Navigation.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import axios from 'axios';
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -9,24 +10,34 @@ import {
 import { cn } from '@/lib/utils';
 import { Category } from '@/types/models';
 
-const Navigation: FC<{ categories: Category[] }> = ({ categories }) => (
-  <NavigationMenu className="font-ali hidden md:flex">
-    <NavigationMenuList>
-      <NavigationMenuItem>
-        <NavigationMenuTrigger className="text-xl">分类</NavigationMenuTrigger>
-        <NavigationMenuContent>
-          <ul className="flex flex-col items-center gap-3 p-5 border border-black">
-            {categories.map((category) => <li key={category.id}>{category.title}</li>)}
-          </ul>
-        </NavigationMenuContent>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild>
-          <div className={cn(navigationMenuTriggerStyle(), 'text-xl')}>关于</div>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-    </NavigationMenuList>
-  </NavigationMenu>
-);
+const Navigation: FC = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get(route('categories.index'));
+      setCategories(response.data.data);
+    })();
+  }, []);
+  return (
+    <NavigationMenu className="font-ali hidden md:flex">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="text-xl">分类</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="flex flex-col items-center gap-3 p-5 border border-black">
+              {categories.map((category) => <li key={category.id}>{category.title}</li>)}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <div className={cn(navigationMenuTriggerStyle(), 'text-xl')}>关于</div>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
 
 export default Navigation;
