0512212cb3ed157d71e2c5fa760e47a7a59127c7
戴日东 <550398445@qq.com>
2023-10-31 15:03:26 +0800

    only install telescope in local

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // only register if not in testing environment
+        if ($this->app->environment('local')) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
