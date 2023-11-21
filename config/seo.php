a84f620c9828b41694e811ad3221d50dbe60aef3
戴日东 <550398445@qq.com>
2023-11-21 16:16:06 +0800

    try fix seo

---
+++ config/seo.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'description' => env('APP_DESCRIPTION', ''),
+    'spiders' => [
+        'baidu' => env('BAIDU_SEO_KEY', ''),
+        'google' => env('GOOGLE_SEO_KEY', ''),
+    ]
+];
--- resources/js/frontend/app.tsx
+++ resources/js/frontend/app.tsx
@@ -3,9 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createInertiaApp } from "@inertiajs/react";
 import resolvePage from "./resolvePage";
 
-const appName =
-  window.document.getElementsByTagName("title")[0]?.innerText ||
-  import.meta.env.VITE_APP_NAME;
+const appName = import.meta.env.VITE_APP_NAME;
 
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
