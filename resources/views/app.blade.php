782ab10d31949d859bd813f1137f9538a91a0407
戴日东 <550398445@qq.com>
2024-09-17 16:47:10 +0800

    change cdn

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
         <meta name="baidu-site-verification" content="{{ config('seo.spiders.baidu') }}" />
         <meta name="google-site-verification" content="{{ config('seo.spiders.google') }}" />
         <link rel="stylesheet" href="https://fonts.font.im/css?family=Press+Start+2P">
-        <link rel="stylesheet" href="https://static.zeoseven.com/cn/297/main/result.css" />
+        <link rel="stylesheet" href="https://fastly.jsdelivr.net/npm/@chinese-fonts/jnjj@2.0.1/dist/JUNJUN/result.css" />
         <link rel="stylesheet" href="https://static.zeoseven.com/cn/69/main/result.css">
 
         {{--        <title inertia>{{ config('app.name', 'Laravel') }}</title>--}}
