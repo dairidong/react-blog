986ede32967d8d7dd9fc1c735796d410d00b606c
戴日东 <550398445@qq.com>
2024-09-17 16:22:34 +0800

    fix cdn

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
         <meta name="baidu-site-verification" content="{{ config('seo.spiders.baidu') }}" />
         <meta name="google-site-verification" content="{{ config('seo.spiders.google') }}" />
         <link rel="stylesheet" href="https://fonts.font.im/css?family=Press+Start+2P">
-        <link rel="stylesheet" href="https://chinese-fonts-cdn.deno.dev/packages/jnjj/dist/JUNJUN/result.css" />
+        <link rel="stylesheet" href="https://static.zeoseven.com/cn/297/main/result.css" />
         <link rel="stylesheet" href="https://static.zeoseven.com/cn/69/main/result.css">
 
         {{--        <title inertia>{{ config('app.name', 'Laravel') }}</title>--}}
